00e4cab88024424f88ab0bf5c97f433ddcf71d35
Philippe Babin <philippe.babin@gmail.com>
2024-03-23 12:51:03 -0400

    Add LCD library

--- src/audio_ampli_mcu/audio_ampli_mcu.ino
+++ src/audio_ampli_mcu/audio_ampli_mcu.ino
@@ -7,6 +7,12 @@
 #include "volume_controller.h"
 #include "audio_input_controller.h"
 
+#include "LCD_Driver.h"
+#include "GUI_Paint.h"
+#include "image.h"
+
+#include <SPI.h>
+
 #define STARTUP_VOLUME_PERCENTAGE 50
 #define TOTAL_TICK_PER_FULL_VOLUME 1024
 
@@ -31,6 +37,26 @@ void setup()
 
   Serial.begin(115200);
   while(!Serial);
+
+  Config_Init();
+  LCD_Init();
+  LCD_Clear(0xffff);
+  Paint_NewImage(LCD_WIDTH, LCD_HEIGHT, 0, WHITE);
+  Paint_Clear(WHITE);
+  Paint_DrawString_EN(30, 34, "Hello papa", &Font24, BLUE, CYAN);
+  
+  Paint_DrawRectangle(125, 10, 225, 58, RED,  DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
+  Paint_DrawLine(125, 10, 225, 58, MAGENTA,   DOT_PIXEL_2X2,LINE_STYLE_SOLID);
+  Paint_DrawLine(225, 10, 125, 58, MAGENTA,   DOT_PIXEL_2X2,LINE_STYLE_SOLID);
+  
+  Paint_DrawCircle(150,100, 25, BLUE,   DOT_PIXEL_2X2,   DRAW_FILL_EMPTY);
+  Paint_DrawCircle(180,100, 25, BLACK,  DOT_PIXEL_2X2,   DRAW_FILL_EMPTY);
+  Paint_DrawCircle(210,100, 25, RED,    DOT_PIXEL_2X2,   DRAW_FILL_EMPTY);
+  Paint_DrawCircle(165,125, 25, YELLOW, DOT_PIXEL_2X2,   DRAW_FILL_EMPTY);
+  Paint_DrawCircle(195,125, 25, GREEN,  DOT_PIXEL_2X2,   DRAW_FILL_EMPTY);
+  
+
+  Paint_DrawImage(gImage_70X70, 20, 80, 70, 70); 
 }
 
 void loop()
