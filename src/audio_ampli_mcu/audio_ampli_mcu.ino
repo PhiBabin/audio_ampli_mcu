f35608c555200261caca0cbbb015fb27ca19e157
Philippe Babin <philippe.babin@gmail.com>
2024-11-02 15:30:59 -0400

    Add instructions on compiling with Arduino CLI to the README

--- src/audio_ampli_mcu/audio_ampli_mcu.ino
+++ src/audio_ampli_mcu/audio_ampli_mcu.ino
@@ -1,6 +1,6 @@
 /// Version of each external libraries (use Library Manager to install them):
 /// - Raspberry Pi Pico/RP2040: 3.7.2
-/// - rp2040-encoder-library: 0.1.1
+/// - rp2040-encoder-library: 0.1.2
 /// - InputDebounce: 1.6.0
 /// - MCP23S17: 0.5.1
 /// - RP2040_PWM: 1.7.0
